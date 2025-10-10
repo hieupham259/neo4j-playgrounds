508c54ef92384be01c91b96754f58cce9275f08b
hieupham <hieupham@maxflowtech.com>
2025-10-10 12:08:42 +0700

    add: add multi_query_transaction.py

--- transaction/session_exe_read.py
+++ transaction/session_exe_read.py
@@ -1,6 +1,19 @@
 from db.connection import Neo4jConnection
 
 
+'''
+
+Create a session. A single session can be the container for multiple queries. Unless created using the with construct, remember to close it when done.
+
+The .execute_read() (or .execute_write()) method is the entry point into a transaction. It takes a callback to a transaction function and an arbitrary number of positional and keyword arguments which are handed down to the transaction function.
+
+The transaction function callback is responsible of running queries.
+
+Use the method Transaction.run() to run queries. Each query run returns a Result object.
+
+Process the result using any of the methods on Result.
+'''
+
 def match_person_nodes(tx, name_filter):
     result = tx.run("""
         MATCH (p:Person) WHERE p.name STARTS WITH $filter
